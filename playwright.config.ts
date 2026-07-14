4edd9f53ddf5fa0296830e98ad6432d080feef6f
Peng Lin <linpeng2027@gmail.com>
2026-07-14 01:47:55 -0400

    test(e2e): align Playwright test discovery

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./e2e",
+  testDir: "./tests/e2e",
   webServer: {
     command: "npm run dev -- --hostname 127.0.0.1 --port 3100",
     url: "http://127.0.0.1:3100",
